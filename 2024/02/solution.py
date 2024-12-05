906177fde8e1f2bf20597cbfacb6613d0a214857
Nicolas Gonzalez <ndgfrs@gmail.com>
2024-12-04 22:41:23 -0500

    chore: update README.md

--- 2024/02/solution.py
+++ 2024/02/solution.py
@@ -2,13 +2,18 @@
 Solution for Advent of Code 2024, day 2.
 https://adventofcode.com/2024/day/2
 
+<<<<<<< HEAD
 Attempted on: 2024-12-20
+=======
+Attempted on: 2024-12-04
+>>>>>>> a37feb7 (style: run mdformat over readme files)
 """
 
 import pathlib
 
 
 def part_one(data: str) -> int:
+<<<<<<< HEAD
     return sum(map(is_safe, data.splitlines()))
 
 
@@ -50,15 +55,40 @@ def valid_level(a: int, b: int, /, *, increasing: bool) -> bool:
 
 
 def part_two(data: str) -> int:
+=======
+    """A brief description about part one.
+
+    Returns
+    -------
+    int:
+        <A brief description about what you are returning>
+    """
+    raise NotImplementedError
+
+
+def part_two(data: str) -> int:
+    """A brief description about part two.
+
+    Returns
+    -------
+    int:
+        <A brief description about what you are returning>
+    """
+>>>>>>> a37feb7 (style: run mdformat over readme files)
     raise NotImplementedError
 
 
 if __name__ == "__main__":
+<<<<<<< HEAD
     data = (
         pathlib.Path(__file__)
         .resolve()
         .parent.joinpath("input.txt")
         .read_text()
     )
+=======
+    current_dir = pathlib.Path(__file__).resolve().parent
+    data = current_dir.joinpath("input.txt").read_text()
+>>>>>>> a37feb7 (style: run mdformat over readme files)
     print("part one:", part_one(data=data))
     print("part two:", part_two(data=data))
