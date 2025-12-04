81a2e078ba55755e69e67fab68578b4be5d4c81a
Nicolas Gonzalez <ndgfrs@gmail.com>
2025-12-04 02:16:56 -0500

    feat(2025): complete 4-2

--- src/bin/04/main.rs
+++ src/bin/04/main.rs
@@ -2,8 +2,12 @@
 
 fn main() {
     let input = include_str!("./input.txt");
-    println!("Part 1: {}", part_one(input));
-    println!("Part 2: {}", part_two(input));
+
+    let solution_one = part_one(input);
+    let solution_two = part_two(input);
+
+    println!("Part 1: {}", solution_one);
+    println!("Part 2: {}", solution_two);
 }
 
 #[must_use]
@@ -81,7 +85,89 @@ fn part_one(input: &str) -> i64 {
 
 #[must_use]
 fn part_two(input: &str) -> i64 {
-    0
+    let mut grid = input
+        .lines()
+        .map(|line| line.as_bytes().to_vec())
+        .collect::<Vec<_>>();
+
+    let mut total = 0;
+
+    let max_rows = grid.len();
+
+    loop {
+        let total_copy = total;
+
+        for (dy, row) in grid.clone().iter_mut().enumerate() {
+            let dy = isize::try_from(dy).unwrap();
+
+            let max_columns = row.len();
+
+            for (dx, value) in row.iter_mut().enumerate() {
+                if value != &b'@' {
+                    print!(".");
+                    continue;
+                }
+
+                let dx = isize::try_from(dx).unwrap();
+
+                let mut adjacent_rolls_of_paper = 0;
+
+                let targets = [
+                    (dy - 1, dx - 1),
+                    (dy - 1, dx),
+                    (dy - 1, dx + 1),
+                    (dy, dx - 1),
+                    (dy, dx + 1),
+                    (dy + 1, dx - 1),
+                    (dy + 1, dx),
+                    (dy + 1, dx + 1),
+                ];
+
+                for (y, x) in targets {
+                    // Ensure the Y coordinate is not out of bounds.
+                    if y < 0 || y >= isize::try_from(max_rows).unwrap() {
+                        continue;
+                    }
+
+                    // Ensure the X coordinate is not out of bounds.
+                    if x < 0 || x >= isize::try_from(max_columns).unwrap() {
+                        continue;
+                    }
+
+                    let x = usize::try_from(x).unwrap();
+                    let y = usize::try_from(y).unwrap();
+
+                    if grid[y][x] == b'@' {
+                        adjacent_rolls_of_paper += 1;
+                    }
+                }
+
+                if adjacent_rolls_of_paper < 4 {
+                    print!("x");
+                    total += 1;
+                    grid[usize::try_from(dy).unwrap()][usize::try_from(dx).unwrap()] = b'.';
+                } else {
+                    print!("@");
+                }
+            }
+
+            #[expect(clippy::print_with_newline)]
+            {
+                print!("\n");
+            }
+        }
+
+        #[expect(clippy::print_with_newline)]
+        {
+            print!("\n");
+        }
+
+        if total == total_copy {
+            break;
+        }
+    }
+
+    total
 }
 
 #[cfg(test)]
@@ -108,6 +194,6 @@ mod tests {
 
     #[test]
     fn part_two_example() {
-        // assert_eq!(part_two(EXAMPLE_INPUT), 3121910778619);
+        assert_eq!(part_two(EXAMPLE_INPUT), 43);
     }
 }
